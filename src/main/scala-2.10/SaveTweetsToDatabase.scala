9ff53b743b92caa90448da0708d330c152a54ccf
Brian Drupieski <brian.drupieski@gmail.com>
2015-11-27 10:32:02 -0500

    change log formatting, add additional logging, small refactor to match and tweet insert logic

--- src/main/scala-2.10/SaveTweetsToDatabase.scala
+++ src/main/scala-2.10/SaveTweetsToDatabase.scala
@@ -1,12 +1,15 @@
 import java.util.concurrent.atomic.AtomicInteger
 
+import slick.dbio.{NoStream, DBIOAction}
 import slick.driver.H2Driver.api._
+
 import twitter4j._
 import AnagramMatchBuilder._
 import Filters._
 
 import org.slf4j.LoggerFactory
 
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
@@ -42,42 +45,47 @@ object SaveTweetsToDatabase {
             TweetDatabaseConfig.db.run(tweetMatchQuery.result) map { (tweets: Seq[Tweet]) =>
 
               val sameTweetsAlreadySavedToDb = tweets.filter(x => x.tweetStrippedText == newTweet.tweetStrippedText)
+
               if (sameTweetsAlreadySavedToDb.nonEmpty) {
-                log.info(
-                  s"ALREADY SAVED: ${newTweet.tweetOriginalText} (${newTweet.tweetStrippedText}) ${sys.props("line.separator")}" +
-                  s"EXISTING DUPLICATE TWEET(S): ${sameTweetsAlreadySavedToDb.map(x => x.tweetOriginalText).mkString(" ")}")
+                log.info(s"ALREADY SAVED: ${newTweet.tweetOriginalText} (${newTweet.tweetStrippedText}) " +
+                  s"EXISTING DUPLICATE(S): ${sameTweetsAlreadySavedToDb.map(x => s"${x.tweetOriginalText} (${x.tweetStrippedText})").mkString(" ")}")
               } else {
-                val actions = if (tweets.nonEmpty) {
+
+                val inserts = ListBuffer[DBIOAction[_, NoStream, Effect.Write]](tweetInsert)
+
+                if (tweets.nonEmpty) {
                   val anagramMatches = tweets.map(buildAnagramMatch(newTweet, _)).filter(isGoodMatch)
-                  val anagramMatchInserts = anagramMatchesTable ++= anagramMatches
-
-                  val matchLog = anagramMatches.map(x => s"IF: ${x.interestingFactor}").mkString(" ")
-                  log.info(s"MATCH ON ${newTweet.tweetOriginalText}: $matchLog")
-
-                  DBIO.seq(tweetInsert, anagramMatchInserts).transactionally
-                } else {
-                  DBIO.seq(tweetInsert)
+                  if (anagramMatches.nonEmpty) {
+                    val anagramMatchInserts = anagramMatchesTable ++= anagramMatches
+                    inserts += anagramMatchInserts
+                    log.info(s"MATCH ON ${newTweet.tweetOriginalText}:" +
+                      s" ${anagramMatches.map(x => s"IF: ${x.interestingFactor}").mkString(" ")}")
+                  }
                 }
 
                 log.debug(s"inserting (${savedTweets.get()}): ${newTweet.tweetOriginalText}")
-                TweetDatabaseConfig.db.run(actions)
+                TweetDatabaseConfig.db.run(DBIO.seq(inserts: _*).transactionally)
                 savedTweets.incrementAndGet()
               }
             }
           }
         }
       }
-      def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = { }
-      def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = { }
+      def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = {
+        log.info(s"STATUS DELETED: ${statusDeletionNotice.getStatusId}")
+      }
+      def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = {
+        log.warn(s"TRACK LIMITATION NOTICE: $numberOfLimitedStatuses")
+      }
       def onException(ex: Exception): Unit = {
-        ex.printStackTrace()
         log.error(ex.getMessage)
         log.error(ex.getStackTraceString)
       }
-      def onScrubGeo(arg0: Long, arg1: Long): Unit = { }
+      def onScrubGeo(arg0: Long, arg1: Long): Unit = {
+        log.info(s"GEO SCRUBBED: $arg0, $arg1")
+      }
       def onStallWarning(warning: StallWarning): Unit = {
-        log.warn("STALL WARNING!")
-        log.warn(warning.toString)
+        log.warn(s"STALL WARNING: ${warning.toString}")
       }
     }
 
